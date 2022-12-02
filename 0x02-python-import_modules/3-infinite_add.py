588c76050a5a966b93c54d08b41dbf6e957c6e9b
tfikadu <tfikadu76@gmail.com>
2022-12-01 17:28:24 -0800

    Task three

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 3-infinite_add.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Fikadu Tessema <tfikadu76@gmail.com>
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
