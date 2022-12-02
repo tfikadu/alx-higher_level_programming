10b262b5308ff93821100ecff2d36a4e2ffeed49
tfikadu <tfikadu76@gmail.com>
2022-12-01 17:18:46 -0800

    Task two

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-args.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Fikadu Tessema <tfikadu76@gmail.com>
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
