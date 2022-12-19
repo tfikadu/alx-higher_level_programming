42eee42856703eceeed593d97b5d3f6d04f327ed
tfikadu <tfikadu76@gmail.com>
2022-12-19 11:58:20 -0800

    Task9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# tfikadu <tfikadu76@gmail.com>
+
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -6,7 +10,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result = result + (a**b) / i
+                result += a ** b / i
         except:
             result = b + a
             break
