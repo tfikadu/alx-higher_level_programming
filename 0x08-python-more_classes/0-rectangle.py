5753d794d78709078c37ef461d2f7994cdab2090
tfikadu <tfikadu76@gmail.com>
2023-01-02 11:44:56 -0800

    All tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built for Alx project 0x08 task 0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""1-rectangle, built for ALX Python project 0x08 task 1.
+"""
+
+
+class Rectangle:
+    """At this stage the class only creates private instance attributes by
+    taking in two arguments.
+    Args:
+        width (int): horizontal dimension of rectangle, defaults to 0
+        height (int): vertical dimension of rectangle, defaults to 0
+    """
+    def __init__(self, width=0, height=0):
+        # attribute assigment here engages setters defined below
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """__width getter.
+        Returns:
+            __width (int): horizontal dimension of rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Args:
+            value (int): horizontal dimension of rectangle
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """__height getter.
+        Returns:
+            __height (int): vertical dimension of rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Args:
+            value (int): vertical dimension of rectangle
+        Attributes:
+            __height (int): vertical dimension of rectangle
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
