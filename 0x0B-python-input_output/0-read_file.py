dd158e05676ded5eea46245670a1bcdc9538e4f8
tfikadu <tfikadu76@gmail.com>
2023-01-10 11:09:21 -0800

    Task fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""read_file module.
+Contains a function that reads a text file.
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r') as f:
+        print(f.read(), end='')
