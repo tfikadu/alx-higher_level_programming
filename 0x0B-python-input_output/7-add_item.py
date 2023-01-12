45ab02e96067d6743fb199ee3fc5753f674e5018
tfikadu <tfikadu76@gmail.com>
2023-01-11 23:43:11 -0800

    Task fixedd

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+'''
+Write a function that writes an
+Object to a text file, using a
+JSON representation
+'''
 
-from sys import argv
 import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-'''
-Write a script that adds all
-arguments to a Python list, and then
-save them to a file
-'''
 
 
-argc = len(argv)
-
-filename = 'add_item.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except BaseException:
-    pass
-for items in range(1, argc):
-    my_list.append(argv[items])
-save_to_json_file(my_list, filename)
+def save_to_json_file(my_obj, filename):
+    '''
+    Writes object to text file using JSON
+    '''
+    if filename is None:
+        return
+    with open(filename, 'w', encoding='utf-8') as f:
+        json_var = json.dump(my_obj, f)
