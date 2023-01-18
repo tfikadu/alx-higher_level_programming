bf41ece180af70161ec591681bda0a281268e023
tfikadu <tfikadu76@gmail.com>
2023-01-18 10:01:46 -0800

    all tasks fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,241 +1,163 @@
 #!/usr/bin/python3
-"""0x0C. Python - Almost a circle, task 2 - 13"""
+"""
+...
+"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Creates rectangle objects with 2 dimensions and offset coordinates.
-    Uses superclass `__init__` to create valid instance id, and sets
-    self vars from args.
-    Args:
-        width (int): x dimension of rectangle
-        height (int): y dimension of rectangle
-        x (int): horizontal offset of rectangle
-        y (int): vertical offset of rectangle
-        id (int): unique identifer for each instance of super()
-    Project task:
-        2. First Rectangle - __init__, getters and setters for `width`,
-            `height`, `x`, `y`
     """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        # attribute assigment here engages setters defined below
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+    ...
+    """
 
-    def __integer_validator(self, attr, value):
-        """Validates incoming argument values for use with internal attributes.
-        Args:
-            attr (str): name of intended attribute assignment
-            value (any): expecting int, but will filter out other types
-        Raises:
-            TypeError: if any incoming value is not (int)
-            ValueError: if any `width` or `height` candidate is <= 0, or if
-                `x` or `y` candidates are < 0
-        Project task:
-            3. Validate attributes - input validation for `width`, `height`,
-                `x`, `y`
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(attr))
-        if attr is 'width' or attr is 'height':
-            if value <= 0:
-                raise ValueError('{} must be > 0'.format(attr))
-        elif attr is 'x' or attr is 'y':
-            if value < 0:
-                raise ValueError('{} must be >= 0'.format(attr))
+        ...
+        """
+        super().__init__(id)
+
+        self.check_integer_parameter(width, 'width')
+        self.check_integer_parameter(height, 'height')
+        self.check_integer_parameter(x, 'x')
+        self.check_integer_parameter(y, 'y')
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
-        """`__width` getter
-        Returns:
-            __width (int): x dimension of rectangle
+        """
+        ...
         """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """Args:
-            value (int): x dimension of rectangle
-        Attributes:
-            __width (int): x dimension of rectangle
+    def width(self, param):
         """
-        self.__integer_validator('width', value)
-        self.__width = value
+        ...
+        """
+        self.check_integer_parameter(param, 'width')
+
+        self.__width = param
 
     @property
     def height(self):
-        """`__height` getter
-        Returns:
-            __height (int): y dimension of rectangle
+        """
+        ...
         """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """Args:
-            value (int): y dimension of rectangle
-        Attributes:
-            __height (int): y dimension of rectangle
+    def height(self, param):
         """
-        self.__integer_validator('height', value)
-        self.__height = value
+        ...
+        """
+        self.check_integer_parameter(param, 'height')
+
+        self.__height = param
 
     @property
     def x(self):
-        """`__x` getter
-        Returns:
-            __x (int): horizontal offset of rectangle
+        """
+        ...
         """
         return self.__x
 
     @x.setter
-    def x(self, value):
-        """Args:
-            value (int): horizontal offset of rectangle
-        Attributes:
-            __x (int): horizontal offset of rectangle
+    def x(self, param):
         """
-        self.__integer_validator('x', value)
-        self.__x = value
+        ...
+        """
+        self.check_integer_parameter(param, 'x')
+
+        self.__x = param
 
     @property
     def y(self):
-        """`__y` getter
-        Returns:
-            __y (int): vertical offset of rectangle
+        """
+        ...
         """
         return self.__y
 
     @y.setter
-    def y(self, value):
-        """Args:
-            value (int): vertical offset of rectangle
-        Attributes:
-            __y (int): vertical offset of rectangle
+    def y(self, param):
         """
-        self.__integer_validator('y', value)
-        self.__y = value
+        ...
+        """
+        self.check_integer_parameter(param, 'y')
+
+        self.__y = param
+
+    def check_integer_parameter(self, value, param):
+        """
+        ...
+        """
+        if type(value) is not int:
+            raise TypeError(param + ' must be an integer')
+
+        if value <= 0 and param in ('width', 'height'):
+            raise ValueError(param + ' must be > 0')
+
+        if value < 0 and param in ('x', 'y'):
+            raise ValueError(param + ' must be >= 0')
 
     def area(self):
-        """Returns area of rectangle as product of `width` and `height`.
-        Returns:
-            `__width` * `__height`
-        Project tasks:
-            4. Area first - public method `area()`
         """
-        return self.width * self.height
+        ...
+        """
+        return self.__width * self.__height
 
     def display(self):
-        """Prints representation of rectangle to stdout using '#'.
-        Attributes:
-            display (str): printed as ASCII art drawing of rectangle
-            __display (str): final value of `display` saved to instance
-                attribute for unit testing
-        Project tasks:
-            5. Display #0 - public method `display()`, only use `width`
-        and `height`
-            7. Display #1 - include use of offset vars `x` and `y`
         """
-        display = ''
-        for row in range(self.y):
-            display += '\n'
-        for row in range(self.height):
-            for column in range(self.x):
-                display += ' '
-            for column in range(self.width):
-                display += '#'
-            if row < self.height - 1:
-                display += '\n'
-        self.__display = display
-        print(display)
+        ...
+        """
+        if self.__y > 0:
+            print('\n' * self.__y, end='')
+
+        for i in range(self.height):
+            if self.__x > 0:
+                print(' ' * self.__x, end='')
+
+            print('#' * self.__width)
 
     def __str__(self):
-        """Returns string with numeric representation of rectangle
-        Returns:
-            '[Rectangle] (<id>) <x>/<y> - <width>/<height>'
-        Project tasks:
-            6. __str__ - `__str__` method
         """
-        return ('[Rectangle] ({:d}) {:d}/'.format(self.id, self.x) +
-                '{:d} - {:d}/{:d}'.format(self.y, self.width, self.height))
+        ...
+        """
+        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
+            self.id, self.x, self.y, self.width, self.height
+        )
 
     def update(self, *args, **kwargs):
-        """Updates attributes in a given order based on variable amount of
-        non-keyword args, or in any order with keyword args.
-        `*args` takes precedence over `**kwargs`: if any non-keyword args are
-        present, keyword args are ignored.
-        Args (non-keyword, in order of appearance in variable list):
-            id (int): identifer for instances of `super()`
-            width (int): x dimension of rectangle
-            height (int): y dimension of rectangle
-            x (int): horizontal offset of rectangle
-            y (int): vertical offset of rectangle
-        Keyword args:
-            any of the above if key matches arg name
-        Raises:
-            TypeError: if amount of arguments given not between 1 and 5 (not
-                in project instructions)
-            KeyError: if keyword not among class and superclass getters
-        Project tasks:
-            8. Update #0 - updates `id`, `width`, `height`, `x`, or `y` based
-                on amount of args using *args
-            9. Update #1 - adds use of **kwargs to access key-worded argments
-                in any order. if *args not empty, **kwargs skipped
         """
-        if len(args) == 0:
-            if len(kwargs) == 0 or len(kwargs) > 5:
-                raise TypeError('Rectangle.update() takes 1 to 5 keyword,' +
-                                ' or 1 to 5 non-keyword arguments')
-            else:
-                for key, value in kwargs.items():
-                    if key == 'id':
-                        self.id = value
-                        Base._Base__assigned_ids.add(value)
-                    elif key == 'width':
-                        self.width = value
-                    elif key == 'height':
-                        self.height = value
-                    elif key == 'x':
-                        self.x = value
-                    elif key == 'y':
-                        self.y = value
-                    else:
-                        raise KeyError('invalid attribute name: ' +
-                                       '{}'.format(key))
-        elif len(args) > 5:
-            raise TypeError('Rectangle.update() takes 1 to 5 keyword,' +
-                            ' or 1 to 5 non-keyword arguments')
-        else:
-            for i, arg in enumerate(args):
-                if i == 0:
-                    if self.id != arg:
-                        self.id = arg
-                        Base._Base__assigned_ids.add(arg)
-                elif i == 1:
-                    self.width = arg
-                elif i == 2:
-                    self.height = arg
-                elif i == 3:
-                    self.x = arg
-                elif i == 4:
-                    self.y = arg
+        ...
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'width', 'height', 'x', 'y']
+
+        if argc > 5:
+            argc = 5
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
 
     def to_dictionary(self):
-        """Creates dictionary representation of self without revealing private
-        attribute names, as would __dict__.
-        Returns:
-            self_dict (dict): custom dictionary of private attribute values
-                populated using getters
-        Project tasks:
-            13. Rectangle instance to dictionary representation
         """
-        self_dict = dict()
-        self_dict['id'] = self.id
-        self_dict['width'] = self.width
-        self_dict['height'] = self.height
-        self_dict['x'] = self.x
-        self_dict['y'] = self.y
-        return self_dict
+        ...
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,156 +1,176 @@
 #!/usr/bin/python3
-"""Unittest for base({..]) after task 1"""
+"""
+A module that test differents behaviors
+of the Base class
+"""
 import unittest
+import pep8
+import os
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    """Tests `Base` class."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Alerts user to any instances of tested classes or subclasses not
-        already cleaned up.
+    """
+    A class to test the Base Class
+    """
+    def test_pep8_base(self):
         """
-        b_obj = Base._Base__nb_objects
-        b_tobj = Base._Base__true_nb_objects
-        b_ids = Base._Base__assigned_ids
-        if b_obj > 1:
-            print('testBase: previous Base counter not reset,' +
-                  'now at: {}'.format(b_obj))
-        if b_tobj > 1:
-            print('testBase: previous total Base counter not reset,' +
-                  'now at: {}'.format(b_tobj))
-        if len(b_ids) > 0:
-            print('testBase: previous Base ids still potentially in' +
-                  'use: {}'.format(b_ids))
-
-    def tearDown(self):
-        """Reinitializes obejct iterator and set of assigned ids.
+        Test that checks PEP8
         """
-        b_ids = Base._Base__assigned_ids
-        Base._Base__nb_objects = 0
-        b_obj = Base._Base__nb_objects
-        Base._Base__true_nb_objects = 0
-        b_tobj = Base._Base__true_nb_objects
-        b_ids.clear()
-        if b_obj > 1:
-            print('testBase: Base counter not reset, now at: {}'.format(b_obj))
-        if b_tobj > 1:
-            print('testBase: total Base counter not reset,' +
-                  ' now at: {}'.format(b_tobj))
-        if len(b_ids) > 0:
-            print('testBase: Base ids still potentially in' +
-                  'use: {}'.format(b_ids))
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
-    def test_id(self):
-        """Task 1. Base class
-        According to project specs, `id` can be assumed to be an integer,
-        so TypeError testing omitted.
+    def test_id_as_positive(self):
         """
-        # ValueError: assigning id of 0
-        self.assertRaises(ValueError, Base, 0)
-        # ValueError: assigning negative ids
-        self.assertRaises(ValueError, Base, -5)
-        # Normal use: Assigning postive id
-        b1 = Base(2)
-        self.assertEqual(b1.id, 2)
-        self.assertEqual(b1.serial, 1)
-        # No arg defaults to id = count of instances w/o manually assigned id
-        b2 = Base()
-        self.assertEqual(b2.id, 1)
-        self.assertEqual(b2.serial, 2)
-        # Further instance w/o arg continues pattern, even if id already used
-        b3 = Base()
-        self.assertEqual(b3.id, 2)
-        self.assertEqual(b3.serial, 3)
-        # Manually assigning id that has already automatically been assigned
-        b4 = Base(2)
-        self.assertEqual(b1.id, 2)
-        self.assertEqual(b1.serial, 1)
-        self.assertEqual(b3.id, 2)
-        self.assertEqual(b3.serial, 3)
-        self.assertEqual(b4.id, 2)
-        self.assertEqual(b4.serial, 4)
+        Test for a positive Base Class id
+        """
+        base_instance = Base(115)
+        self.assertEqual(base_instance.id, 115)
+        base_instance = Base(67)
+        self.assertEqual(base_instance.id, 67)
+
+    def test_id_as_negative(self):
+        """
+        Test for a negative Base Class id
+        """
+        base_instance = Base(-91)
+        self.assertEqual(base_instance.id, -91)
+        base_instance = Base(-4)
+        self.assertEqual(base_instance.id, -4)
+
+    def test_id_as_none(self):
+        """
+        Test for a None Base Class id
+        """
+        base_instance = Base()
+        self.assertEqual(base_instance.id, 1)
+        base_instance = Base(None)
+        self.assertEqual(base_instance.id, 2)
+
+    def test_string_id(self):
+        base_instance = Base('Monty Python')
+        self.assertEqual(base_instance.id, 'Monty Python')
+        base_instance = Base('Python is cool')
+        self.assertEqual(base_instance.id, 'Python is cool')
 
     def test_to_json_string(self):
-        """15. Dictionary to JSON string"""
-        # Normal use: 1 arg (list) of (dict)
-        d_list = [{'id': 10}, {'id': 15}]
-        self.assertEqual(Base.to_json_string(d_list),
-                         '[{"id": 10}, {"id": 15}]')
-        # Empty list
-        self.assertEqual(Base.to_json_string([]), '[]')
-        # None list
-        self.assertEqual(Base.to_json_string(None), '[]')
-        # deprecated use: list not of dicts
-        lst = [2, 3, 4]
-        self.assertEqual(Base.to_json_string(lst), '[2, 3, 4]')
-        # TypeError: no args
-        self.assertRaises(TypeError, Base.to_json_string)
-        d_list2 = [{'id': 20}]
-        # TypeError: 2+ args
-        self.assertRaises(TypeError, Base.to_json_string, d_list, d_list2)
-
-    def test_save_to_file(self):
-        """16. JSON string to file
-        AttributeError: .to_dictionary() not in `Base`, only Square/Rectangle.
-        Testing of `save_to_file` done in `TestRectangle` and `TestSquare`.
         """
-        # Empty list
-        # None list
-        # arg not an iterable type
-        # arg not a list, but tuple or set
-        # arg list contents not Squares or Rectangles (no to_dictionary())
-        # class of list objects does not match class that called save_to_file
-        # no args
-        # 2+ args
-        pass
-
-    def test_from_json_string(self):
-        """17. JSON string to dictionary"""
-        # Normal use: 1 arg (str)
-        a = Base.from_json_string('[{"id": 10}, {"id": 15}]')
-        self.assertEqual(a, [{'id': 10}, {'id': 15}])
-        # Empty str
-        b = Base.from_json_string('')
-        self.assertEqual(b, [])
-        # None str
-        c = Base.from_json_string(None)
-        self.assertEqual(c, [])
-        # TypeError: no args
-        self.assertRaises(TypeError, Base.from_json_string)
-        # TypeError: 2+ args
-        self.assertRaises(TypeError, Base.from_json_string, a, b)
-
-    def test_create(self):
-        """18. Dictionary to Instance
-        TypeError: __init__ for `Base` takes 1 arg, and `create` uses 2 to
-        make `temp`.
-        Testing of `create` done in `TestRectangle` and `TestSquare`.
+        Test the to_json_string method
         """
-        # no args
-        # with any non-keyword args
-        # dict has bad key
-        # dict has too many keys
-        # dict has no keys
-        pass
+        rect_instance = Rectangle(10, 7, 2, 8, 70)
+        rect_data = re1.to_dictionary()
+        json_data = Base.to_json_string([rect_data])
+        self.assertEqual(type(json_data), str)
+
+    def test_empty_to_json_string(self):
+        """
+        Test for a empty data on the to_json_string method
+        """
+        empty_data = []
+        json_data = Base.to_json_string(empty_data)
+        self.assertEqual(json_data, "[]")
+
+        empty_data = None
+        json_data = Base.to_json_string(empty_data)
+        self.assertEqual(json_data, "[]")
+
+    def test_instance(self):
+        """
+        Test a Base Class instance
+        """
+        base_instance = Base()
+        self.assertEqual(type(base_instance), Base)
+        self.assertTrue(isinstance(base_instance, Base))
+
+    def test_to_json_string(self):
+        """
+        Test a normal to_json_string functionality
+        """
+        rect_data = {'id': 31, 'x': 14, 'y': 11, 'width': 3, 'height': 3}
+        json_data = Base.to_json_string([rect_data])
+
+        self.assertTrue(isinstance(rect_data, dict))
+        self.assertTrue(isinstance(json_data, str))
+        self.assertCountEqual(
+            json_data,
+            '{["id": 31, "x": 14, "y": 11, "width": 3, "height": 3]}'
+        )
+
+    def test_wrong_to_json_string(self):
+        """
+        Test a wrong functionality of the
+        to_json_string method
+        """
+        json_data = Base.to_json_string(None)
+        self.assertEqual(json_data, "[]")
+
+        warn = ("to_json_string() missing 1 required positional argument: " +
+                "'list_dictionaries'")
+
+        with self.assertRaises(TypeError) as msg:
+            Base.to_json_string()
+
+        self.assertEqual(warn, str(msg.exception))
+
+        warn = "to_json_string() takes 1 positional argument but 2 were given"
+
+        with self.assertRaises(TypeError) as msg:
+            Base.to_json_string([{43, 87}], [{22, 17}])
+
+        self.assertEqual(warn, str(msg.exception))
+
+    def test_wrong_save_to_file(self):
+        """
+        Test the save_to_file method
+        """
+        with self.assertRaises(AttributeError) as msg:
+            Base.save_to_file([Base(1), Base(2)])
+
+        self.assertEqual(
+             "'Base' object has no attribute 'to_dictionary'",
+             str(msg.exception)
+        )
 
     def test_load_from_file(self):
-        """19. File to instances
-        `load_from_file` uses `create`, and so must be tested in subclasses
-        Testing of `create` done in `TestRectangle` and `TestSquare`.
         """
-        # Normal use: file with JSON string of instances as dicts
-        # empty file
-        # empty list in file
-        # file has other content / not in JSON
-        # file inaccessible
-        # mixed square and rectangle objects represented in string
-        # any dict has bad key
-        # any dict has too many keys
-        # any dict has no keys
-        pass
+        Test the load_from_file method
+        """
+        if os.path.exists("Base.json"):
+            os.remove("Base.json")
 
-if __name__ == '__main__':
-    unittest.main()
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
+
+        rect_output = Rectangle.load_from_file()
+        self.assertEqual(rect_output, [])
+
+        square_output = Square.load_from_file()
+        self.assertEqual(square_output, [])
+
+        warn = "load_from_file() takes 1 positional argument but 2 were given"
+
+        with self.assertRaises(TypeError) as msg:
+            Rectangle.load_from_file('Monty Python')
+
+        self.assertEqual(warn, str(msg.exception))
+
+    def test_create(self):
+        """
+        Test the create method
+        """
+        with self.assertRaises(TypeError) as msg:
+            warn = Rectangle.create('Monty Python')
+
+        self.assertEqual(
+            "create() takes 1 positional argument but 2 were given",
+            str(msg.exception)
+        )
