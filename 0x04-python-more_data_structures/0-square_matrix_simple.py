c7159c41b9c1395e001f4abedb80bdabc5532e6c
tfikadu <tfikadu76@gmail.com>
2023-03-14 11:52:54 -0700

    Zero task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    if len(matrix) > 0:
-        for elems in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2, elems)))
-
-    return 
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
