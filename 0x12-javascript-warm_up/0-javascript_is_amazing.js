2a2505be64fcf7e511e23a48e1c569551845ed69
tfikadu <tfikadu76@gmail.com>
2023-03-15 07:13:18 +0000

    Task fixed

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Print string “Javascript is amazing”
+// Print string “JavaScript is amazing”
 
-const myVar = 'Javascript is amazing';
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
