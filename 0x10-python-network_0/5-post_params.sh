bcc851f489320dc63ec512af9a51be617ee5727e
tfikadu <tfikadu76@gmail.com>
2023-03-30 11:53:49 -0700

    Task six

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#curl sends POST req to URL, display response body
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=tfikadu76@gmail.com&subject=I will always be here for PLD" "$1"
