2e698ebe1d9cec1fa6ad1eb8baae8db417896c88
tfikadu <tfikadu76@gmail.com>
2023-03-30 18:59:01 -0700

    Task fixed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
-
+# Displays the body of the response of a curl POST request
+curl -sLX PUT -d "user_id=98" -H "Origin:NewSchool" 0.0.0.0:5000/catch_me
