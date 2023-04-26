59a0734d2a7794f5ba88a62633dde6eeab94dbf7
tfikadu <tfikadu76@gmail.com>
2023-04-25 19:56:54 -0700

    Task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], err => {
-  if (err) {
-    console.log(err);
-  }
-});
