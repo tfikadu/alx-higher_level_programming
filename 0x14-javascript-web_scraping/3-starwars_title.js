cdc9ef61c6a15f106cddb571d11f3eb9e5e4e003
tfikadu <tfikadu76@gmail.com>
2023-04-27 06:25:40 -0700

    Web-scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.alx-tools.com/api/films/:id'.concat(process.argv[2]);
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starWarsUri, function (_err, _res, body) {
   body = JSON.parse(body);
